afde8fd2d3a98ba0fdb8f938f1c0a4b6841aab4b
Kieran Bessert <61402238+kabessert1994@users.noreply.github.com>
2020-06-09 12:15:44 -0700

    Update Kieran C++ CS132 Lab 07 - Insertion Sort.cpp

--- Kieran C++ CS132 Lab 07 - Insertion Sort.cpp
+++ Kieran C++ CS132 Lab 07 - Insertion Sort.cpp
@@ -15,13 +15,13 @@ namespace
 	/***********************CONSTANT VARIABLES***********************/
 	const string IN_FILE_NAME = "..\\unsorted127.txt";      // Constant variable for storing name of the input file
 	const string OUT_FILE_NAME = "unsorted127_sorted.txt";  // Constant variable for storing name of the output file
-	const int MAX_SIZE_INT = 127;							// The maximum number of integers from the file
+	const int MAX_SIZE_INT = 127;				// The maximum number of integers from the file
 }
 class Unsorted
 {
 public:
 	int value;
-	class Unsorted* nextNumber;								// Self-referential structure
+	class Unsorted* nextNumber;				// Self-referential structure
 };
 int main()
 {
@@ -42,35 +42,36 @@ int main()
 
 	for (int ix = 0; ix <= MAX_SIZE_INT; ix++)              // Import the values from the input file and sort
 	{
-		newNode = new Unsorted;								// Dynamically create new node
-		intCount++;											// Keep track of how many values imported
-		in_stream >> newNode->value;						// Store the value from the file in a node
-		if (headPtr == NULL)								// Check if no list exists
+		newNode = new Unsorted;				// Dynamically create new node
+		intCount++;					// Keep track of how many values imported
+		in_stream >> newNode->value;			// Store the value from the file in a node
+		if (headPtr == NULL)				// Check if no list exists
 		{
-			headPtr = newNode;								// New node becomes head of list
+			headPtr = newNode;			// New node becomes head of list
 			headPtr->nextNumber = NULL;
 		}
-		else if (newNode->value < headPtr->value)			// Add a value to the FRONT of the list
+		else if (newNode->value < headPtr->value)	// Add a value to the FRONT of the list
 		{
-			newNode->nextNumber = headPtr;					// Must not lose this reference
-			headPtr = newNode;								// Revise headPtr to reference newNode
+			newNode->nextNumber = headPtr;		// Must not lose this reference
+			headPtr = newNode;			// Revise headPtr to reference newNode
 		}
-		else if (newNode->value > headPtr->value)			// Add a value to the END of the list
+		else if (newNode->value > headPtr->value)	// Add a value to the END of the list
 		{
-			movingPtr = headPtr;							// Find end of list
+			movingPtr = headPtr;			// Find end of list
 			do
 			{
-				if (movingPtr->nextNumber == NULL)			// Add a value to the END of the list
+				if (movingPtr->nextNumber == NULL)	// Add a value to the END of the list
 				{ 
-					movingPtr->nextNumber = newNode;		// Place newNode to END of list
-					newNode->nextNumber = NULL;				// Mark as last node
+					movingPtr->nextNumber = newNode;// Place newNode to END of list
+					newNode->nextNumber = NULL;	// Mark as last node
 					break;
 				}
-				else if ((movingPtr->nextNumber)->value		// Add a value to the MIDDLE of the list
-										> newNode->value)
+				else if ((movingPtr->nextNumber)->value	> newNode->value)
+									// Add a value to the MIDDLE of the list
+
 				{ 
 					newNode->nextNumber = movingPtr->nextNumber;// Must not lose this reference
-					movingPtr->nextNumber = newNode;		// Revise to reference newNode
+					movingPtr->nextNumber = newNode;// Revise to reference newNode
 					break;
 				}
 				else
@@ -81,31 +82,31 @@ int main()
 		}
 	}
 	in_stream.close();
-	out_stream.open(OUT_FILE_NAME);							// Open the output file
-	if (out_stream.fail())									// Checks to see if open function fails
+	out_stream.open(OUT_FILE_NAME);					// Open the output file
+	if (out_stream.fail())						// Checks to see if open function fails
 	{
 		cout << "Output file opening failed.\n";
 		in_stream.close();
 		return(-1);
 	}
-	for (int ix = intCount; ix >= 0; ix--)					// Print Nodes out and release to freestore
+	for (int ix = intCount; ix >= 0; ix--)				// Print Nodes out and release to freestore
 	{
-		if (headPtr == NULL)								// Check if no list exists
+		if (headPtr == NULL)					// Check if no list exists
 		{
 			cout << "\n\t*** END OF LIST ***\n";
 		}
 		else if ((headPtr->nextNumber) != NULL)
 		{
-			movingPtr = headPtr;								// Start at head of list
-			headPtr = headPtr->nextNumber;						// Move headPtr to next item in list as new head
-			cout << headPtr->value << endl;						// Output to screen
-			out_stream << headPtr->value << endl;				// Output to file
-			delete movingPtr;									// Return memory to freestore
+			movingPtr = headPtr;				// Start at head of list
+			headPtr = headPtr->nextNumber;			// Move headPtr to next item in list as new head
+			cout << headPtr->value << endl;			// Output to screen
+			out_stream << headPtr->value << endl;		// Output to file
+			delete movingPtr;				// Return memory to freestore
 		}
 		else if ((headPtr->nextNumber) == NULL)
 		{
-			movingPtr = headPtr;								// Start at head of list
-			headPtr = NULL;										// Set headPtr to null
+			movingPtr = headPtr;				// Start at head of list
+			headPtr = NULL;					// Set headPtr to null
 		}
 	}
 	out_stream.close();
