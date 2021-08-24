091fc6ea349a69618784a39148d359eab5c07ebb
Kieran Bessert <61402238+kabessert1994@users.noreply.github.com>
2021-08-24 14:04:13 -0700

    Create Kieran SQL Basic Query 3.sql

---
+++ Kieran SQL Basic Query 3.sql
@@ -0,0 +1,2 @@
+# Display the first name and last name of employees sorted based on their age (listing the youngest employees first)
+select first_name, last_name from employees order by -birth_date;
